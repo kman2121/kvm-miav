aa24d9e48ef8b22b1877c4f55d349e2cc3003d0d
Vijay Pillai <vijay.pillai397@gmail.com>
2017-12-01 18:40:37 -0500

    added tabbed view

--- mobile/kvm-miav/containers/EnterJobScreen.js
+++ mobile/kvm-miav/containers/EnterJobScreen.js
@@ -2,23 +2,61 @@ import React from 'react';
 import {
   StyleSheet,
   Text,
-  View
+  View,
+  Dimensions
 } from 'react-native';
+import {
+    TabViewAnimated,
+    TabBar,
+    SceneMap
+} from 'react-native-tab-view';
+import { MiniMap,
+    JunkRequest,
+    MoveRequest
+ } from '../components';
 
-import { MiniMap } from '../components';
+ const initialLayout = {
+   height: 0,
+   width: Dimensions.get('window').width,
+ };
 
-export class EnterJobScreen extends React.Component {
+const FirstRoute = () => (<JunkRequest />);
+const SecondRoute = () => (<MoveRequest />)
+
+export class EnterJobScreen extends React.PureComponent {
+  state = {
+    index: 0,
+    routes: [
+      { key: 'first', title: 'Moving' },
+      { key: 'second', title: 'Junk Hauling' },
+    ],
+  };
+  _handleIndexChange = index => this.setState({ index });
+
+  _renderHeader = props => <TabBar {...props} />;
+
+  _renderScene = SceneMap({
+    first: FirstRoute,
+    second: SecondRoute,
+  });
+
   render() {
     return (
-      <View style={styles.container}>
-        <View style={styles.mapContainer}>
-          <MiniMap />
+        <View style={styles.container}>
+            <View style={styles.mapContainer}>
+                <MiniMap />
+            </View>
+            <View style={styles.formContainer}>
+                <TabViewAnimated
+                    style={styles.container}
+                    navigationState={this.state}
+                    renderScene={this._renderScene}
+                    renderHeader={this._renderHeader}
+                    onIndexChange={this._handleIndexChange}
+                    initialLayout={initialLayout}
+                />
+            </View>
         </View>
-
-        <View style={styles.formContainer}>
-          <Text>TODO: this should have a tabbed view with the moving/junk forms</Text>
-        </View>
-      </View>
     );
   }
 }
